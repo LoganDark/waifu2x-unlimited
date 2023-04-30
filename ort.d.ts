caf51bcd0ce07c0e66c5c2877ae2a0107950511a
LoganDark <git@logandark.mozmail.com>
2023-04-29 19:03:11 -0700

    Fix ort.d.ts

--- ort.d.ts
+++ ort.d.ts
@@ -1,10 +1,2 @@
-declare global {
-	const ort: typeof import('onnxruntime-common')
-
-	namespace ort {
-		type InferenceSession = import('onnxruntime-common').InferenceSession
-		type TypedTensor<T> = import('onnxruntime-common').TypedTensor<T>
-	}
-}
-
-export {}
+export * from 'onnxruntime-common'
+export as namespace ort
